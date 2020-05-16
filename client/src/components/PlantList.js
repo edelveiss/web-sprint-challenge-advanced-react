3e043a059e7ae85db85799e625908b4f7f1cba4e
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2020-05-15 19:15:38 -0700

    searchbar and pinkMode are added

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -30,27 +30,33 @@ export default class PlantList extends Component {
   render() {
     return (
       <main className="plant-list">
-        {this.state?.plants?.map((plant) => (
-          <div className="plant-card" key={plant.id}>
-            <img className="plant-image" src={plant.img} alt={plant.name} />
-            <div className="plant-details">
-              <h2 className="plant-name">{plant.name}</h2>
-              <p className="plant-scientific-name">{plant.scientificName}</p>
-              <p>{plant.description}</p>
-              <div className="plant-bottom-row">
-                <p>${plant.price}</p>
-                <p>☀️ {plant.light}</p>
-                <p>💦 {plant.watering}x/month</p>
+        {this.state?.plants
+          ?.filter((item) => {
+            return item.name
+              .toLowerCase()
+              .includes(this.props.text.toLowerCase());
+          })
+          .map((plant) => (
+            <div className="plant-card" key={plant.id}>
+              <img className="plant-image" src={plant.img} alt={plant.name} />
+              <div className="plant-details">
+                <h2 className="plant-name">{plant.name}</h2>
+                <p className="plant-scientific-name">{plant.scientificName}</p>
+                <p>{plant.description}</p>
+                <div className="plant-bottom-row">
+                  <p>${plant.price}</p>
+                  <p>☀️ {plant.light}</p>
+                  <p>💦 {plant.watering}x/month</p>
+                </div>
+                <button
+                  className="plant-button"
+                  onClick={() => this.props.addToCart(plant)}
+                >
+                  Add to cart
+                </button>
               </div>
-              <button
-                className="plant-button"
-                onClick={() => this.props.addToCart(plant)}
-              >
-                Add to cart
-              </button>
             </div>
-          </div>
-        ))}
+          ))}
       </main>
     );
   }
